302f9011bf1e7b57f460e8572c423dfa1fae0f5d
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-09-09 17:50:13 +0600

    Implement store api functionalities

---
+++ app/api/store/dashboard/route.js
@@ -0,0 +1,45 @@
+import authSeller from "@/middlewares/authSeller";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// Get dashboard data for seller (total orders, total earnings, total products)
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+    const storeId = await authSeller(userId);
+
+    // Get all orders for the seller
+    const orders = await prisma.order.findMany({
+      where: { storeId },
+    });
+
+    // Get all products with ratings for the seller
+    const products = await prisma.product.findMany({
+      where: { storeId },
+    });
+
+    const ratings = await prisma.rating.findMany({
+      where: { productId: { in: products.map((product) => product.id) } },
+      include: { user: true, product: true },
+    });
+
+    const dashboardData = {
+      ratings,
+      totalOrders: orders.length,
+      totalEarnings: Math.round(
+        orders.reduce((acc, order) => acc + order.total, 0)
+      ),
+      totalProducts: products.length,
+    };
+
+    return NextResponse.json({ dashboardData }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        error: error.code || error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/api/store/data/route.js
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+// Get store info & store products
+export async function GET(request) {
+  try {
+    // Get store username from query params
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get("username").toLowerCase();
+
+    if (!username) {
+      return NextResponse.json({ error: "Missing username" }, { status: 400 });
+    }
+
+    // Get store info and inStock products with ratings
+    const store = await prisma.store.findUnique({
+      where: { username },
+      include: {
+        Product: { include: { ratings: true } },
+      },
+    });
+
+    if (!store) {
+      return NextResponse.json({ error: "Store not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ store }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        error: error.code || error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
