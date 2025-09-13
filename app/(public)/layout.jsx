22cfacd898d5e2323a1c2098d2eae4b39b5f557d
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-09-13 06:27:49 +0600

    Implement cart functionality

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -2,16 +2,34 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
 import { fetchProducts } from "@/lib/features/product/productSlice";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PublicLayout({ children }) {
   const dispatch = useDispatch();
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  const { cartItems } = useSelector((state) => state.cart);
 
   useEffect(() => {
     dispatch(fetchProducts({}));
   }, []);
+
+  useEffect(() => {
+    if (user?.id) {
+      dispatch(fetchCart({ getToken }));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user?.id) {
+      dispatch(uploadCart({ getToken }));
+    }
+  }, [cartItems]);
   return (
     <>
       <Banner />
--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -35,7 +35,7 @@ export async function POST(request) {
 }
 
 // Get User Cart
-export async function POST(request) {
+export async function GET(request) {
   try {
     const { userId } = getAuth(request);
 
